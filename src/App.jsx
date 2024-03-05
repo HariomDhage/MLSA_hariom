2761969f282084c147a2f2878269c0aa9fc8c5db
Hariom Dhage <130223726+HariomDhage@users.noreply.github.com>
2024-03-06 00:47:00 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hariom Dhage",
+  title: "Web Designer and Content Creator",
+  email: "hariom.dhage@studentambassadors.com",
+  gitHub: "hariomdhage",
+  instagram: "",
+  linkedIn: "hariomdhage",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "hariom",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
